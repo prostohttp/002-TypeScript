2a6833c37aa3ddc0afb1d755316b649221e076e1
Morozov Mikhail <memphis.sail@gmail.com>
2023-10-14 19:10:22 +1000

    v1

---
+++ BooksRepository.ts
@@ -0,0 +1,11 @@
+export abstract class BooksRepository {
+  abstract createBook(book: Book): void
+  abstract getBook(id: number): Book
+  abstract getBooks(): Book[]
+  abstract updateBook(book: Book): void
+  abstract deleteBook(id: number): void
+}
+
+class Book{
+
+}
